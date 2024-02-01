143ecc39a2d13bbfc26ef0434dbb0e0342a00d1b
unknown <Sudaval@PC-GG-00.Sudaval.com>
2024-02-01 15:30:09 -0400

    pdf arreglar patro

---
+++ api/PDF/bd/BD.php
@@ -0,0 +1,13 @@
+<?php
+//function conexion(){
+$host = "localhost";
+$user = "root";
+$contraseña = "";
+$database = "bqef_2";
+$conn = mysqli_connect($host,$user,$contraseña,$database);
+//return $conn;
+//}
+//if (conexion()) {
+//	echo "conectado";
+//}else{ echo "no conectado";
+//}
