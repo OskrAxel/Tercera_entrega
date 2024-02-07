043774dd7cc0f6d9625545288c897d07732191bd
unknown <oskr3qsi@gmail.com>
2024-02-07 01:53:19 -0400

    radio eva

--- src/Pages/Per/Evalua.js
+++ src/Pages/Per/Evalua.js
@@ -11,7 +11,7 @@ import {
 } from "reactstrap";
 import { Table, Button } from "reactstrap";
 import * as FaIcons from "react-icons/fa";
-import axios from "axios";
+import axios, { Axios } from "axios";
 import "../Bec/bec.scss";
 import { Link } from "react-router-dom";
 
@@ -19,6 +19,7 @@ function Evalua() {
   const baseUrl = "http://localhost:80/api/bec/";
   const [data, setData] = useState([]);
   const [modalEditar, setModalEditar] = useState(false);
+  const [modalEliminar, setModalEliminar] = useState(false);
   const [usuarioSeleccionado, setusuarioSeleccionado] = useState({
     id: "",
     nombre: "",
@@ -26,12 +27,32 @@ function Evalua() {
     email: "",
     contrasena: "",
     celular: "",
-    carrera: "",
-    anio: "",
-    nom_pad: "",
-    nom_mad: "",
+    nota_eva: "",
   });
+  ////
+  var radioState = false;
+  function test(element) {
+    if (radioState == false) {
+      check();
+      radioState = true;
+    } else {
+      uncheck();
+      radioState = false;
+    }
+  }
+  function check() {
+    document.getElementById("radioBtn").checked = true;
+  }
+  function uncheck() {
+    document.getElementById("radioBtn").checked = false;
+  }
+  ////
+  const [eva, setEva] = useState(1);
 
+  const cambioRadioEva = (e) => {
+    setEva(e.target.value);
+  };
+  ////
   const handleChange = (e) => {
     const { name, value } = e.target;
     setusuarioSeleccionado((prevState) => ({
@@ -43,6 +64,9 @@ function Evalua() {
 
   const abrirCerrarModalEditar = () => {
     setModalEditar(!modalEditar);
+  };
+  const abrirCerrarModalEliminar = () => {
+    setModalEliminar(!modalEliminar);
   };
 
   const peticionGet = async () => {
@@ -94,13 +118,14 @@ function Evalua() {
   };
   /////
   const seleccionarUsuario = (Usuario, caso) => {
-    abrirCerrarModalEditar();
+    setusuarioSeleccionado(Usuario);
+
+    caso === "Editar" ? abrirCerrarModalEditar() : abrirCerrarModalEliminar();
   };
 
   useEffect(() => {
     peticionGet();
   }, []);
-
   return (
     <div id="main_content">
       <div className="tra">
@@ -110,8 +135,7 @@ function Evalua() {
         <div id="subt">
           <Link
             to={"http://localhost:80/api/PDF/reporte_pdf_becarios.php"}
-            target="_blank"
-          >
+            target="_blank">
             <Button color="primary" size="lg">
               <FaIcons.FaFileDownload /> Reporte
             </Button>
@@ -127,6 +151,7 @@ function Evalua() {
               <th>Correo</th>
               <th>Telf/Cel</th>
               <th>Acciones</th>
+              <th>Evaluación</th>
             </tr>
           </thead>
           <tbody>
@@ -141,11 +166,11 @@ function Evalua() {
                 <td>
                   <button
                     className="btn btn-secondary"
-                    onClick={() => seleccionarUsuario(Usuario, "Editar")}
-                  >
+                    onClick={() => seleccionarUsuario(Usuario, "Editar")}>
                     EVALUAR
                   </button>
                 </td>
+                <td>{Usuario.nota_eva}</td>
               </tr>
             ))}
           </tbody>
@@ -153,52 +178,83 @@ function Evalua() {
 
         <Modal isOpen={modalEditar}>
           <ModalHeader
-            style={{ color: "white", background: "rgba(18, 80, 61, .85)" }}
-          >
+            style={{ color: "white", background: "rgba(18, 80, 61, .85)" }}>
             Evaluar Becario
           </ModalHeader>
           <ModalBody>
             <div className="form-group">
-              <label>Nombres: </label>
+              <label>Nombre Becario: </label>
               <br />
               <input
                 type="text"
                 className="form-control"
                 name="nombre"
                 onChange={handleChange}
-                value={usuarioSeleccionado && usuarioSeleccionado.nombre}
+                value={
+                  usuarioSeleccionado &&
+                  usuarioSeleccionado.nombre +
+                    " " +
+                    usuarioSeleccionado.apellido
+                }
               />
               <br />
-              <label>Apellidos: </label>
-              <br />
-              <input
-                type="text"
-                className="form-control"
-                name="apellido"
-                onChange={handleChange}
-                value={usuarioSeleccionado && usuarioSeleccionado.apellido}
-              />
-              <br />
+              {/* <Input className="mb-3" name="becarios" type="select">
+                {data.map((Usuario) => (
+                  <option key={Usuario.id} value={Usuario.id}>
+                    {Usuario.nombre} {Usuario.apellido}
+                  </option>
+                ))}
+              </Input> */}
               <FormGroup tag="fieldset">
-                <legend>Radio Buttons</legend>
-                <FormGroup check>
-                  <Input name="radio1" type="radio" />{" "}
-                  <Label check>
-                    Option one is this and that—be sure to include why it‘s
-                    great
+                <legend>
+                  Seleccione las opciones: /Posibilidad de entregar el informe a
+                  tiempo
+                </legend>
+                <FormGroup>
+                  <Input
+                    name="radio1"
+                    type="radio"
+                    checked={eva == 1 ? true : false}
+                    onChange={cambioRadioEva}
+                  />{" "}
+                  <Label for="radio1">Asistencia a las reuniones</Label>
+                </FormGroup>
+                <FormGroup>
+                  <Input
+                    name="radio2"
+                    type="radio"
+                    checked={eva == 2 ? true : false}
+                    onChange={cambioRadioEva}
+                  />{" "}
+                  <Label for="radio2">Puntualidad</Label>
+                </FormGroup>
+                <FormGroup>
+                  <Input
+                    name="radio3"
+                    type="radio"
+                    checked={eva == 3 ? true : false}
+                    onChange={cambioRadioEva}
+                  />{" "}
+                  <Label for="radio3">Compañerismo</Label>
+                </FormGroup>
+                <FormGroup>
+                  <Input
+                    name="radio4"
+                    type="radio"
+                    checked={eva == 4 ? true : false}
+                    onChange={cambioRadioEva}
+                  />{" "}
+                  <Label for="radio4">
+                    Cumplimiento reglamento institucional
                   </Label>
                 </FormGroup>
-                <FormGroup check>
-                  <Input name="radio1" type="radio" />{" "}
-                  <Label check>
-                    Option two can be something else and selecting it will
-                    deselect option one
-                  </Label>
-                </FormGroup>
-                <FormGroup check disabled>
-                  <Input disabled name="radio1" type="radio" />{" "}
-                  <Label check>Option three is disabled</Label>
-                </FormGroup>
+                <input
+                  type="radio"
+                  name="name"
+                  id="radioBtn"
+                  onClick={() => test(this)}
+                />{" "}
+                Radio
               </FormGroup>
             </div>
           </ModalBody>
@@ -209,8 +265,7 @@ function Evalua() {
             {"   "}
             <button
               className="btn btn-danger"
-              onClick={() => abrirCerrarModalEditar()}
-            >
+              onClick={() => abrirCerrarModalEditar()}>
               Cancelar
             </button>
           </ModalFooter>
